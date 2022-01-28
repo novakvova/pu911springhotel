107ae5e3a26541946f28c8e9a3fe6681d8a16cc6
novakvova <novakvova@gmail.com>
2022-01-28 11:46:12 +0200

    Add show data

--- src/main/java/hotel/controllers/HomeController.java
+++ src/main/java/hotel/controllers/HomeController.java
@@ -1,18 +1,27 @@
 package hotel.controllers;
 
 import hotel.entities.Hotel;
+import hotel.entities.Region;
+import hotel.repositories.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 public class HomeController {
+    private final RegionRepository regionRepository;
+
+    @Autowired
+    public HomeController(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
 
     @GetMapping("/")
-    public Hotel index() {
-        Hotel hotel =new Hotel();
-        hotel.setName("Баня");
-        return hotel;
+    public List<Region> index() {
+        return regionRepository.findAll();
     }
 }
