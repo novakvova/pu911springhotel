aa0708f45a83a760db83af7a91d9a176cc4c8115
novakvova <novakvova@gmail.com>
2022-02-02 10:27:10 +0200

    Id goog pagable

--- src/main/java/hotel/controllers/BadgeController.java
+++ src/main/java/hotel/controllers/BadgeController.java
@@ -5,6 +5,10 @@ import hotel.entities.Region;
 import hotel.mapper.RegionMapper;
 import hotel.repositories.BadgeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,12 @@ public class BadgeController {
     @GetMapping("/{id}")
     public Badge getById(@PathVariable("id") int id) {
         return regionRepository.findById(id).get();
+    }
+
+    @GetMapping("/all")
+    public Page<Badge> getById() {
+        Sort sort = Sort.by("Id");
+        Pageable pageable = PageRequest.of(0,10,sort);
+        return regionRepository.findAll(pageable);
     }
 }
--- src/main/java/hotel/repositories/BadgeRepository.java
+++ src/main/java/hotel/repositories/BadgeRepository.java
@@ -2,8 +2,9 @@ package hotel.repositories;
 
 import hotel.entities.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BadgeRepository extends JpaRepository<Badge, Integer> {
+public interface BadgeRepository extends JpaRepository<Badge, Integer>, JpaSpecificationExecutor<Badge> {
 }
